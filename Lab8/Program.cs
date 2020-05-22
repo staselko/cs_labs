a5293a67d50c5521334cd104203d41ffdab7059b
staselko <57151154+staselko@users.noreply.github.com>
2020-05-22 18:44:52 +0300

    added labs 1-8

---
+++ Lab8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = new Action();
+            action.AddOperation("mod", (x, y) => x % y);
+            action.RemoveOperation("addition");
+            action.GetNameOperations(Display);
+            double result = action.DoOperation("mod", 6.4724, 6.46426);
+            Display(result.ToString());
+            action.RemoveOperation("mod");
+            action.RemoveOperation("subtraction");
+            action.RemoveOperation("multipcation");
+            action.RemoveOperation("Division");
+            action.GetNameOperations();
+
+        }
+
+        public static void Display(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
